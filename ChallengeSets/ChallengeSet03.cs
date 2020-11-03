ffceb587756fc74ea8e498fa1daafd1f4fe472aa
Ben Sands <m3gap0ky0@gmail.com>
2020-11-02 21:12:18 -0700

    finished cs3 Divide

--- ChallengeSets/ChallengeSet03.cs
+++ ChallengeSets/ChallengeSet03.cs
@@ -106,6 +106,11 @@ namespace ChallengeSets
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
             throw new NotImplementedException();
         }
 
